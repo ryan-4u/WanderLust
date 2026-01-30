678fa085dd379658cc80f13a5287022129837b03
Aaryan <aaryanofficial64@gmail.com>
2026-01-30 17:54:04 +0530

    Index and Show route

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 const express = require("express") ;
 const app = express() ;
-
 const mongoose = require("mongoose") ;
-
 const Listing = require("./models/listing.js") ;
+const path = require("path")
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust" ;
 main()
@@ -18,22 +17,40 @@ async function main() {
     await mongoose.connect(MONGO_URL) ;
 }
 
+app.set("view engine" , "ejs" ) ;
+app.set("views" , path.join( __dirname , "views" ));
+app.use( express.urlencoded( {extended : true} ) ) ; // to parse data from route
+
 app.get("/" , (req,res) => {
     res.send("Hi , i am Groot !") ;
 });
 
-app.get("/testListing" , async (req,res) => {
-  let sampleListing = new Listing({
-    title : "My Villa " ,
-    description : "By the beach" ,
-    price : 4700 ,
-    location : "Calanguate , Goa " ,
-    country : "India"
-  });
-  await sampleListing.save() ;
-  console.log("sample was saved");
-  res.send("successful testing") ;
+//index route
+app.get("/listing" , async (req,res) => {
+  const allListings = await Listing.find({})
+  res.render("listings/index.ejs" , { allListings }) ;
+});
+
+//show route
+app.get("/listings/:id", async (req,res) => {
+  const {id} = req.params ;
+  const listing = await Listing.findById(id) ;
+  res.render("listings/show.ejs", {listing} ) ;
 }) ;
+
+
+// app.get("/testListing" , async (req,res) => {
+//   let sampleListing = new Listing({
+//     title : "My Villa " ,
+//     description : "By the beach" ,
+//     price : 4700 ,
+//     location : "Calanguate , Goa " ,
+//     country : "India"
+//   });
+//   await sampleListing.save() ;
+//   console.log("sample was saved");
+//   res.send("successful testing") ;
+// }) ;
 
 app.listen( 8080 , () => {
     console.log(" server is listening.. ") ;
