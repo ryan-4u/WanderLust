a2059f17584896872491e160e9f6f622d849afae
Aaryan <aaryanofficial64@gmail.com>
2026-01-30 16:17:44 +0530

    Listing model and adding sample data for testing

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ const app = express() ;
 
 const mongoose = require("mongoose") ;
 
+const Listing = require("./models/listing.js") ;
+
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust" ;
 main()
   .then( (res) => {
-    console.log("connected to our db wanderlust")
+    console.log("connected to our db wanderlust") ;
   })
   .catch( (err) => {
     console.log(err) ;
@@ -19,6 +21,19 @@ async function main() {
 app.get("/" , (req,res) => {
     res.send("Hi , i am Groot !") ;
 });
+
+app.get("/testListing" , async (req,res) => {
+  let sampleListing = new Listing({
+    title : "My Villa " ,
+    description : "By the beach" ,
+    price : 4700 ,
+    location : "Calanguate , Goa " ,
+    country : "India"
+  });
+  await sampleListing.save() ;
+  console.log("sample was saved");
+  res.send("successful testing") ;
+}) ;
 
 app.listen( 8080 , () => {
     console.log(" server is listening.. ") ;
