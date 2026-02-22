3c0d1be7071a2e34b0272f664d94db25e9980a70
Aaryan <aaryanofficial64@gmail.com>
2026-02-22 14:07:54 +0530

    automatic login after signing up

--- routes/user.js
+++ routes/user.js
@@ -8,14 +8,19 @@ router.get("/signup" ,(req,res) =>{
     res.render("users/signup.ejs") ;
 })
 
-router.post("/signup" ,wrapAsync( async (req,res) => {
+router.post("/signup" ,wrapAsync( async (req,res,next) => {
     try{
         let {username,email,password} = req.body ;
         const newUser = new User({ email,username }) ;
         const registeredUser = await User.register(newUser , password) ;
         console.log(registeredUser);
+        req.login( registeredUser , (err) => {
+            if(err) { 
+                return next(err)
+            }
         req.flash("success","Welcome to Wanderlust !")
-        res.redirect("/listings"); 
+        res.redirect("/listings");    
+        });
     } catch(e) {
         req.flash("error", e.message) ;
         res.redirect("/signup");
