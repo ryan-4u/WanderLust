aa69212a52982d0c7f1fd4e18dbb7f458acd7f28
Aaryan <aaryanofficial64@gmail.com>
2026-02-22 19:21:14 +0530

    smooth flow : redirecting user after login to intented page

--- middleware.js
+++ middleware.js
@@ -1,7 +1,16 @@
 module.exports.isLoggedIn = (req,res,next) => {
     if( !req.isAuthenticated() ){
+        req.session.redirectUrl = req.originalUrl ; // storing intended path by user 
         req.flash("error","you must be logged in to create list") ;
         return res.redirect("/login");
+    }
+    next() ;
+}
+
+// saving original path to locals from session
+module.exports.saveRedirectUrl = (req,res,next) => {
+    if( req.session.redirectUrl){
+        res.locals.redirectUrl = req.session.redirectUrl ;
     }
     next() ;
 }
