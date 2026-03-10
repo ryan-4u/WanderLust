3889d37fa98a3959eb414841538adddd0efbd8c4
Aaryan <aaryanofficial64@gmail.com>
2026-03-10 15:19:29 +0530

    Databaseis ONLINE via MongoDB Atlas

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const methodOverride = require("method-override") ;
 const ejsMate = require("ejs-mate") ;
 const ExpressError = require("./utils/ExpressError.js");
 const session = require("express-session") ;
+const MongoStore = require('connect-mongo').default ;
 const flash = require("connect-flash") ;
 const passport = require("passport") ;
 const LocalStrategy = require("passport-local") ;
@@ -20,7 +21,9 @@ const listingRouter = require("./routes/listing.js") ;
 const reviewRouter = require("./routes/review.js") ;
 const userRouter = require("./routes/user.js") ;
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust" ;
+// const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust" ;
+const dbUrl = process.env.ATLASDB_URL ;
+
 main()
   .then( (res) => {
     console.log("connected to our db wanderlust") ;
@@ -30,7 +33,7 @@ main()
   });
   
 async function main() {
-    await mongoose.connect(MONGO_URL) ;
+    await mongoose.connect(dbUrl) ;
 }
 
 app.set("view engine" , "ejs" ) ;
@@ -40,8 +43,21 @@ app.use(methodOverride("_method")) ;
 app.engine( "ejs" , ejsMate) ;
 app.use(express.static(path.join(__dirname,"/public"))) ;
 
+const store = MongoStore.create({
+  mongoUrl: dbUrl ,
+  crypto: {
+    secret : "mysupersecretcode"
+  } ,
+  touchAfter : 24 * 3600 ,
+});
+
+store.on( "error" , (err) => {
+  console.log("ERROR in mongo session store" , err) ;
+}) ;
+
 // using session
 const sessionOptions = {
+  store ,
   secret : "mysupersecretcode" ,
   resave : false ,
   saveUninitialized : true ,
@@ -63,15 +79,10 @@ passport.use( new LocalStrategy( User.authenticate() ));
 passport.serializeUser( User.serializeUser() ) ;
 passport.deserializeUser( User.deserializeUser() ) ;
 
-app.use(async (req, res, next) => {
+app.use((req, res, next) => {
   res.locals.success = req.flash("success");
   res.locals.error = req.flash("error");
-  if (req.user) {
-    const User = require("./models/user.js");
-    res.locals.currUser = await User.findById(req.user._id).select("username email favorites");
-  } else {
-    res.locals.currUser = null;
-  }
+  res.locals.currUser = req.user || null;
   next();
 });
 
